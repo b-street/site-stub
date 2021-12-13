681ca101eb6565a2e1dbcf99f6e05340ca39d832
Michael Smyers <michael@smyers.net>
2021-12-12 23:53:14 -0800

    fixes

--- src/pages/cit1x.js
+++ src/pages/cit1x.js
@@ -1,6 +1,8 @@
 import ProductPage from "../components/ProductPage";
+import Sitemap from "../sitemap";
 
 export default function Cit1x() {
+    if (!Sitemap.forPage('cit1x')) return <></>
     return (
         <ProductPage id={'cit1x'} />
     )
